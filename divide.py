7be1f9ff79c31d8a476487102e20e99f103eb39a
Annliza <nnanliza@gmail.com>
2023-10-31 12:35:04 +0300

    initial Commit
    Commiting for the first time while creating the remote repo

---
+++ divide.py
@@ -0,0 +1,7 @@
+# Divide two numbers 
+def division(num1,num2):
+    return num1/num2
+
+print(division(4,2))
+
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from divide import division
+class Test(unittest.TestCase):
+    def testdivide(self):
+        self.assertEqual((2/1),2)
+
+if __name__ ==  "main":
+    unittest.main()
