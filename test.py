ab5c346cde0828d55be649aa205bf06878cc343c
Annliza <nnanliza@gmail.com>
2023-10-31 14:45:26 +0300

    Correct test
    Correcting tests for running update

--- test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from divide import division
-class Test(unittest.TestCase):
-    def testdivide(self):
-        self.assertEqual((2/1),2)
-
-if __name__ ==  "main":
-    unittest.main()
--- test_divide.py
+++ test_divide.py
@@ -1,3 +1,4 @@
+import pytest
 from divide import division
 def testdivide():
         assert division (64,8) == 8
